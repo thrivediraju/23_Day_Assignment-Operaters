36857c79d92870a80dce9a59130e150b0b42f278
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-06-15 20:02:12 +0530

    Add files via upload

---
+++ type.py
@@ -0,0 +1,29 @@
+# a=[1,2,3,4,5,6]
+# print(a)
+
+# a=int(input())
+# b=int(input())
+# print(a+b)
+
+# a,b,c=[float(x) for x in input().split(',')]
+# print(a+b+c)
+
+# a,b=1,4
+# print(type(b))
+
+# a=6,7
+# print(type(a))
+
+# s=[float(x) for x in input().split()]
+# print(sum(s))
+
+# import math as m
+# p=[float(y) for y in input().split()]
+# print(m.prod(p))
+
+# import math as m
+# print(m.factorial(4))
+# print(m.factorial(0))
+# print(m.factorial(3))
+# print(m.factorial(5))
+# print(m.factorial(6))
