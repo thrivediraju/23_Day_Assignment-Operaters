6d4f6ade59add73df9458ea8138a5c03d75b4d5f
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-06-15 20:02:31 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,14 @@
+# i=1
+# while(i<10):
+#     print('Hi',i)
+#     i+=1
+
+# start,end=[int(x) for x in input().split()]
+# while(start<=end):
+#     print('Hey'+str(start))
+#     start+=1
+
+# start,end=[int(x) for x in input().split()]
+# while(start<=end):
+#     print(start)
+#     start+=2
