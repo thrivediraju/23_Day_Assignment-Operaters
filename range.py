bd3a6f45867a69f87c55d7ce355b261f1af2e98d
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-06-15 20:01:41 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,38 @@
+# for i in range(5):
+#     print(i)
+
+# for i in range(5):
+#     print(i,end=' ')
+
+# for i in range(1,6):
+#  print(i)
+
+# for i in range(10,31):
+#  print(i,end=' ')
+
+# for i in range(1,10,2):
+#     print(i)
+
+# for i in range(0,51,2):
+#     print(i,end=' ')
+
+# for i in range(0,20,4):
+#     print(i)
+
+# for x in range(10,0,-1):
+#     print(x)
+
+# for x in range(40,10,-2):
+#     print(x,end=' ')
+
+# start,end,step=[int(x) for x in input('Enter the start,end and step value:').split()]
+# for i in range(start,end,step):
+#     print(i)
+
+# a=[1,2,3.9,3.10,True,None]
+# for i in a:
+#     print(i)
+
+# b=10,20,1.1,'hello'
+# for i in b:
+#     print(i)
